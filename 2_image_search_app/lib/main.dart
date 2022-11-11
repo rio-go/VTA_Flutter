9b2bae4bafc2d0b6203eb4a04baeb0c30f1ca034
Rio Ikeda <ikeda.rio28@gmail.com>
2022-11-11 14:27:26 +0900

    first commit

---
+++ 2_image_search_app/lib/main.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: PixabayPage(),
+    );
+  }
+}
+
+class PixabayPage extends StatefulWidget {
+  const PixabayPage({super.key});
+
+  @override
+  State<PixabayPage> createState() => _PixabayPageState();
+}
+
+class _PixabayPageState extends State<PixabayPage> {
+  List imageList = [];
+
+  Future<void> fetchImages(String text) async {
+    Response response = await Dio().get(
+      'https://pixabay.com/api/?key=21046333-b3d0b6f1757a235b778bb1f89&q=$text&image_type=photo&pretty=true',
+    );
+    imageList = response.data['hits'];
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImages('');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          decoration: const InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+          ),
+          onFieldSubmitted: (text) {
+            fetchImages(text);
+          },
+        ),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemCount: imageList.length,
+        itemBuilder: (context, index) {
+          Map<String, dynamic> image = imageList[index];
+          return InkWell(
+            onTap: () async {
+              Directory dir = await getTemporaryDirectory();
+              Response response = await Dio().get(
+                image['webformatURL'],
+                options: Options(
+                  responseType: ResponseType.bytes,
+                ),
+              );
+              File imageFile = await File('${dir.path}/image.png')
+                  .writeAsBytes(response.data);
+              // ignore: deprecated_member_use
+              await Share.shareFiles([imageFile.path]);
+            },
+            child: Stack(
+              children: [
+                Image.network(image['previewURL']),
+                Text(
+                  image['likes'].toString(),
+                  style: const TextStyle(
+                    backgroundColor: Colors.black,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
